e6b245d545e3edc223c136721df671ce740ad256
omripfeifer <154254204+omripfeifer@users.noreply.github.com>
2023-12-20 18:07:27 +0200

    AddTwo.java

--- AddTwo.java
+++ AddTwo.java
@@ -1,7 +1,7 @@
-public class AddTwo {
-	public static void main(String[] args) {
+public class AddTwo{
+	public static void main(String[] args){
 	int num1 = Integer.parseInt(args[0]);
 	int num2 = Integer.parseInt(args[1]);
-	System.out.println(num1 + " + " + num2 + " = " + (num1+num2));
+	System.out.println( num1 + " + " + num2 + " = " + (num1+num2));
 	}
 }
