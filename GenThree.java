0d586cd01516c4deb41b239320bf0e50d143af9e
omripfeifer <154254204+omripfeifer@users.noreply.github.com>
2023-12-22 12:45:24 +0200

    Update GenThree.java

--- GenThree.java
+++ GenThree.java
@@ -1,19 +1,21 @@
-public class GenThree {
-    public static void main(String[] args){
-        int a = Integer.parseInt(args[0]);
-        int b = Integer.parseInt(args[1]);
-        int x = (int) (Math.random() * (a - b + 1 ) + b);
-        int y = (int) (Math.random() * (a - b + 1 ) + b);
-        int z = (int) (Math.random() * (a - b + 1 ) + b);
-        System.out.println(x);
-        System.out.println(y);
-        System.out.println(z);
-        if (x <= y && x <= z) {
-            System.out.println(" The minimal generated number was : " + x);
-        } else if (y < x && y <= z) {
-            System.out.println(" The minimal generated number was : " + y);
-        } else {
-            System.out.println(" The minimal generated number was " + z);
-        }
+import java.util.Random;
+
+class GenThree {
+    public static void main(String [] args) {
+
+        // Parse command-line args
+        int num1 = Integer.parseInt(args[0]);
+        int num2 = Integer.parseInt(args[1]);
+
+        // Generate three random numbers in the range [a, b) and find the minimal number
+        Random random = new Random();
+        int random1 = random.nextInt(Math.max(num1, num2) - Math.min(num1, num2)) + Math.min(num1, num2);
+        int random2 = random.nextInt(Math.max(num1, num2) - Math.min(num1, num2)) + Math.min(num1, num2);
+        int random3 = random.nextInt(Math.max(num1, num2) - Math.min(num1, num2)) + Math.min(num1, num2);
+        int min_num = Math.min(Math.min(random1, random2), random3);
+
+        // Print the generated random numbers and the minimal number
+        System.out.println(random1 + "\n" + random2 + "\n" + random3);
+        System.out.println("The minimal generated number was " + min_num);
     }
 }
