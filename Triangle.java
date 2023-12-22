7119ebba2134f2edccff69a9b12f74366c708335
omripfeifer <154254204+omripfeifer@users.noreply.github.com>
2023-12-22 07:25:58 +0200

    Update Triangle.java

--- Triangle.java
+++ Triangle.java
@@ -4,6 +4,6 @@ public class Triangle {
 	int b = Integer.parseInt(args[1]);
 	int c = Integer.parseInt(args[2]);
 	boolean isTriangle = a + b > c && a + c > b && b + c > a;
-	System.out.println(a + ", " + b + ", " + c + " : " + isTriangle); 
+	System.out.println(a + ", " + b + ", " + c + ": " + isTriangle); 
    }
 } 
