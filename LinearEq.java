60abd7e471fb278a4a19712eb31217e9e8b45ba0
omripfeifer <154254204+omripfeifer@users.noreply.github.com>
2023-12-22 12:44:59 +0200

    Update LinearEq.java

--- LinearEq.java
+++ LinearEq.java
@@ -1,13 +1,17 @@
-public class LinearEq{
-	public static void main(String[] args){
-	// define all  parametrs 
-	double a = Double.parseDouble(args[0]);
-	double b = Double.parseDouble(args[1]);
-        double c = Double.parseDouble(args[2]);
-        // define x 
+class LinearEq {
+    public static void main(String [] args) {
+
+        // Parse and declare command-line arguments
         double x = 0.0;
-        x = (c-b)/a;
-        System.out.println(a + " * x + " + b + " = " + c  );
-        System.out.println( "x = " + x );
-	}
+        double a = Double.parseDouble(args[0]);
+        double b = Double.parseDouble(args[1]);
+        double c = Double.parseDouble(args[2]);
+
+        // Solve the linear equation
+        x = (c - b) / a;
+
+        // Print the equation and the solution
+        System.out.println(a + " * " + "x " + "+ " + b + " = " + c);
+        System.out.println("x" + " = " + x);
+    }
 }
