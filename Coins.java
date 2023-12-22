760c198e39909a09c1a8b31e169608ee0551b8f2
omripfeifer <154254204+omripfeifer@users.noreply.github.com>
2023-12-22 12:39:34 +0200

    Update Coins.java

--- Coins.java
+++ Coins.java
@@ -1,8 +1,13 @@
-public class Coins{
-	public static void main(String[] args){
+public class Coins {
+	public static void main(String [] args){
+		
 	int coins = Integer.parseInt(args[0]);
+
+	// Calculate the number of quarters and cents	
 	int quarters = coins / 25; 
 	int cent = coins % 25;
+		
+	// print result
 	System.out.println("Use " + quarters + " quarters and " + cent + " cents");
 	}
 }
