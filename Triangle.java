2fc96665410b1201e84363e601132f37cb710384
omripfeifer <154254204+omripfeifer@users.noreply.github.com>
2023-12-22 12:44:37 +0200

    Update Triangle.java

--- Triangle.java
+++ Triangle.java
@@ -1,9 +1,15 @@
-public class Triangle{
-	public static void main(String[] args){
-	int a = Integer.parseInt(args[0]);
-	int b = Integer.parseInt(args[1]);
-	int c = Integer.parseInt(args[2]);
-	boolean isTriangle = a + b > c && a + c > b && b + c > a;
-	System.out.println(a + ", " + b + ", " + c + ": " + isTriangle); 
-   }
-} 
+class Triangle {
+    public static void main(String [] args) {
+        
+        // parse command-line args
+        int side1 = Integer.parseInt(args[0]);
+        int side2 = Integer.parseInt(args[1]);
+        int side3 = Integer.parseInt(args[2]);
+
+        //check if sides can be form a triangle
+        boolean check_if_triangle = (side1 + side2 > side3) && (side2 + side3 > side1) && (side3 + side1 > side2);
+
+        // print the sides and the result
+        System.out.println (side1 + ", " + side2 + ", " + side3 + ": " + check_if_triangle);
+    }
+}
